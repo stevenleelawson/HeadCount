e38687fcbf9a01235d1a96f4b5c5f20848e155fb
Amy Russell <russell.amyh@gmail.com>
2018-03-18 09:33:56 -0600

    most tests for app passing, boilerplate for other app tests setup

--- src/App.test.js
+++ src/App.test.js
@@ -43,9 +43,9 @@ describe('App', () => {
     expect(wrapper.state('districtArray').length).toBe(181)
     expect(wrapper.state('selectedCards').length).toBe(0)
     expect(wrapper.state('averages')).toBe(null)
-  })
 
-    
+  })
+   
   describe('retrieveData', () => {
     it('resets districtArray in app state when called with user input', () => {
       wrapper.instance().retrieveData('Colorado')
@@ -54,25 +54,64 @@ describe('App', () => {
   })
 
   describe('handleClick', () => {
+    const mockEvent1 = { target: {id: "COLORADO"} }
+    const mockEvent2 = { target: {id: "ACADEMY 20"} }
+
     it('should add a card to selectedCards if no cards exist in the array', () => {
-      const mockEvent = {
-        target: {id: "COLORADO"}
-      }
-      wrapper.instance().handleClick(mockEvent);
+      wrapper.instance().handleClick(mockEvent1);
       expect(wrapper.state('selectedCards').length).toBe(1)
     })
 
     it('should add another card to selectedCards if one card exists in the array', () => {
       expect(wrapper.state('selectedCards').length).toBe(1)
-
-      const mockEvent2 = {
-        target: {id: "ACADEMY 20"}
-      }
-
       wrapper.instance().handleClick(mockEvent2);
       expect(wrapper.state('selectedCards').length).toBe(2)
     })
 
-    it('should remove a card from state')
+    it('should remove a card from selectedCards if clicked twice', () => {
+      wrapper.setState({
+        selectedCards: [
+          { location: 'COLORADO',
+            data: { 
+              '2004': 0.24,
+              '2005': 0.278,
+              '2006': 0.337 } 
+          }]})
+      expect(wrapper.state('selectedCards').length).toBe(1)
+      wrapper.instance().handleClick(mockEvent1);
+      expect(wrapper.state('selectedCards').length).toBe(0)  
+    })
+
+    it('should remove the first, and add a new object if two objects are already in selectedCards', () => {
+      wrapper.setState({
+        selectedCards: [
+          { location: 'COLORADO',
+            data: { 
+              '2004': 0.24,
+              '2005': 0.278,
+              '2006': 0.337 } 
+          },
+          { location: 'AGATE 300',
+            data: { 
+              '2004': 1,
+              '2005': 0,
+              '2006': 1 }
+          }]})
+      expect(wrapper.state('selectedCards').length).toBe(2)
+      wrapper.instance().handleClick(mockEvent2);
+      // expect(wrapper.state('selectedCards')).toMatchObject("ACADEMY 20")
+    })
+
+    it('should not add more than two cards to selectedCards', () => {
+
+    })
+
+    it('should call get AverageOfSelected if there are 1 or more cards in selectedArray', () => {
+
+    })
+
+    describe('getAverageOfSelected', () => {
+      it('should set state with averages of two locations')
+    })
   })
 }) 

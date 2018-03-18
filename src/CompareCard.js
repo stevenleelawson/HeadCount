f242afe1368bde9f6049cf1c60b86345d21d53c8
Steven Lawson <unluckygamblermusic@gmail.com>
2018-03-17 18:03:11 -0600

    header created and styling added and proptypes updated

--- src/CompareCard.js
+++ src/CompareCard.js
@@ -1,21 +1,25 @@
 import React from 'react';
-import './Card.css'
+import './CompareCard.css'
 import PropTypes from 'prop-types';
 
 const CompareCard = (props) => {
   const avgEntries = Object.entries(props.averages)
   const magicObj = avgEntries.map(entry =>
-    <div>
-      <h1>{entry[0]}:</h1>
-      <h1>{entry[1]}</h1> 
+    <div className='compare-card'>
+      <h1 className='location'>{entry[0]}:</h1>
+      <h1 className='data'>{entry[1]}</h1>
     </div>
   )
   // const avgValues = Object.values(props.averages)
   return (
-    <div>
+    <div className='compare-card-container'>
+      <h2 className='location'>COMPARED DISTRICTS</h2>
       {magicObj}
     </div>
   )
 }
 
+CompareCard.propTypes = {
+  averages: PropTypes.object.isRequired
+}
 export default CompareCard;
---
+++ src/Header.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import SearchBar from './SearchBar';
+import './Header.css';
+import PropTypes from 'prop-types';
+
+const Header = ({filterSchools}) => {
+  return (
+    <div className='header'>
+      <h1 className='title'>headcount 2.0</h1>
+      <SearchBar filterSchools={filterSchools}/>
+    </div>
+  )
+}
+
+Header.propTypes = {
+  filterSchools: PropTypes.func.isRequired
+}
+export default Header;
